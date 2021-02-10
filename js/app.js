909168e68c4d0a638f737a49785289a484002676
Sajeda-Araidah <sajedaaraidah@gmail.com>
2021-02-10 20:32:50 +0200

    code201 || lab03

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict'
-
-
+let score =0;
 let YourName = prompt("What is Your Name ?");
 alert("Welcom  " + YourName + ' ' + "Lets play with us ");
 
@@ -8,6 +7,7 @@ let YourAge = prompt("Are You Age Between 20 and 30")
 YourAge = YourAge.toLowerCase();;
 if (YourAge == 'yes' || YourAge == 'y') {
     alert("Welcom To you");
+    score+=1;
 } else if (YourAge == 'no' || YourAge == 'n') {
     alert("You age not Age Between 20 and 30");
 }
@@ -26,6 +26,7 @@ while (numOfTry) {
     } else {
         alert("you guess  the number it's  " + hiddenum + '  Correct Answer');
         break;
+        score+=1;
     }
 
     numOfTry -= 1;
@@ -35,12 +36,14 @@ while (numOfTry) {
         break;
     }
 }
+alert("The Correct answer is "+hiddenum+"  ") ; 
 
 
 let YourBackground = prompt("Are you Backgroud IT");
 YourBackground = YourBackground.toLowerCase();
 if (YourBackground == 'yes' || YourBackground == 'y') {
     alert("DEVELOPER");
+    score+=1;
 } else if (YourBackground == 'no' || YourBackground == 'n') {
     alert("NOT DEVELOPER");
 }
@@ -51,6 +54,7 @@ let knowe = prompt("Did You hear about ASAC ?");
 knowe = knowe.toLowerCase();
 if (knowe == 'yes' || knowe == 'y') {
     alert("Amazing");
+    score+=1;
 } else if (knowe == 'no' || knowe == 'n') {
     alert("NOT YET ");
 }
@@ -61,28 +65,42 @@ console.log(knowe);
 let attempts = 6;
 let listOfCouress = ['html', 'css', 'sql', 'ccna', 'js', 'jqury'];
 let courses = prompt("Guess What ASAC's couress!! ");
-while (attempts) {
+myloop: while (attempts) {
+    attempts -= 1;
     for (let i = 0; i < listOfCouress.length; i++) {
         if (courses === listOfCouress[i]) {
             alert('you guess  the couress its  '+ listOfCouress[i] + '  Correct Answer');
-            console.log("Roqia");
-            break;
+            score+=1;
+            break myloop; 
         }
-        attempts -= 1;
-        courses = prompt('please try again you have : ' + attempts + ' remaining!  ');
-    }
-    if (!attempts) {
-        break;
-    }
+        
+          }
+    courses = prompt('please try again you have : ' + attempts + ' remaining!  ');
+
 }
+alert("The Correct answer is "+listOfCouress+"  ") ; 
 
 
 
 let c = prompt("Do you learn new coures?");
 c = c.toLowerCase();
 if (c == 'yes' || c == 'y') {
-    alert("Teel Me About this courses");
+    alert("Tell Me About this courses");
+    score+=1;
 } else if (c == 'no' || c == 'n') {
     alert("You Sould That ");
 }
 console.log(c);
+
+
+let haveCar = prompt("Do You Have Car");
+haveCar =  haveCar.toLowerCase();
+if ( haveCar == 'yes' ||  haveCar== 'y') {
+    alert("You Have Car");
+    score+=1;
+} else if ( haveCar == 'no' ||  haveCar == 'n') {
+    alert("You  Don't Have Car");
+}
+console.log( haveCar);
+
+alert('you answer '+ score + '  /7 Correct Answer Thank You for Your Time ');
